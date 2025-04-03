a06ce13e20b20efc2063dc1cbbc820a5985998ba
itsjh1242 <itsjh1242@gmail.com>
2025-04-03 09:52:44 +0900

    feat: update react version to 19 latest

--- src/components/layout/root.tsx
+++ src/components/layout/root.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export const RootLayout: React.FC = () => {
   return (
-    <main className="flex flex-col w-full min-h-screen bg-gray-50 text-gray-900">
+    <main className="flex min-h-screen w-full flex-col">
       <div className="flex-1">
         <Outlet />
       </div>
